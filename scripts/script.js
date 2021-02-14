9996f8a8cde1bd35d1dbf10804d5c43644ff692f
yoonho0628 <davidjeong0628@gmail.com>
2021-02-13 16:46:19 -0800

    Implement product-list and add items

--- scripts/script.js
+++ scripts/script.js
@@ -3,12 +3,25 @@
 window.addEventListener('DOMContentLoaded', () => {
   if (localStorage.getItem('items') === null) {
     fetchJSON('https://fakestoreapi.com/products').then(data => {
-      localStorage.setItem('items', JSON.stringify(data));
+      localStorage.setItem('items', JSON.stringify(data))
+    }).then(() => {
+      createList();
     });
+  } else {
+    createList();
   }
 });
 
 async function fetchJSON(url) {
   const response = await fetch(url);
   return response.json();
+}
+
+async function createList() {
+  const items = JSON.parse(localStorage.getItem('items'));
+  const productList = document.getElementById('product-list');
+
+  for (let item of items) {
+    productList.innerHTML += `<product-item title="${item.title}" price="$${String(item.price)}" img-src="${item.image}"></product-item>`;
+  }
 }
